88d58523ce34a02559b6c00ced6148418debbb8b
rudi <chickenmastermadras@gmail.com>
2024-01-24 23:20:22 +0100

    Einzelnes Event abrufen funktioniert

--- client/src/app/_pages/single-event/single-event.component.ts
+++ client/src/app/_pages/single-event/single-event.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { events } from 'src/app/_data/events';
 import { Event } from 'src/app/_models/events';
+import { Event2 } from 'src/app/_models/events';
 import { CartService } from 'src/app/_services/cart.service';
 
 @Component({
@@ -14,7 +15,10 @@ export class SingleEventComponent implements OnInit {
 
   eventId: number;
   event: Event;
+  event2: Event2[] = [];
 
+  baseUrl2 = 'http://localhost:3000/eventg';
+  
   formgroup: FormGroup;
 
   constructor(
@@ -23,7 +27,7 @@ export class SingleEventComponent implements OnInit {
     private router: Router
   ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(){
     this.eventId = +this.route.snapshot.paramMap.get('eventId');
     this.event = events.find(element => element.id === this.eventId);
 
@@ -35,8 +39,23 @@ export class SingleEventComponent implements OnInit {
     setInterval(() => {
       console.log(this.cartService.getCart());
     }, 3000)
+
+
+    this.baseUrl2 = `${this.baseUrl2}?id=${this.eventId}`;
+    await this.loadEvent(this.baseUrl2);
+    console.log('Event', this.event2);
   }
 
+  async loadEvent(url) {
+    this.event2 = await this.getAllEvents(url);
+    //console.log(this.events2.Typ);
+  }
+
+  async getAllEvents(url: URL): Promise<Event2[]> {
+    const response = await fetch(url);
+      return await response.json();
+  }
+ 
   public addToCart() {
     console.log('add to cart');
 
